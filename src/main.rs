a1b562afadc29e4f8e0242cbd2e38ca78e45532d
Junxuan <njunxuan@gmail.com>
2019-07-28 23:32:12 +0800

    Serialize multiple structs to BSON file

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,64 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
+use std::io::Cursor;
 
 #[derive(Debug, Serialize, Deserialize)]
 enum Direction {
     North,
     South,
     East,
-    West
+    West,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Move {
-    direction: Direction
+    direction: Direction,
 }
 
 fn main() -> Result<(), ()> {
-    let a = Move {
-        direction: Direction::South
-    };
-
-    let serialized = ron::ser::to_string(&a).unwrap();
-    let serialized_bytes = serialized.into_bytes();
-    println!("{:?}", serialized_bytes);
-
-    let content = std::str::from_utf8(&serialized_bytes).unwrap();
-    println!("{:?}", content);
-
-    // let deserialized: Move = ron::de::from_str(&serialized).unwrap();
-    // println!("deserialized = {:?}", deserialized);
+    serialize();
+    deserialize();
+    // let deserialized = bson::decode_document(&mut Cursor::new(&buf[..]));
+    // println!("{:?}", deserialized);
 
     Ok(())
+}
+
+fn deserialize() {
+    let mut file = File::open("bson").unwrap();
+    for i in 1..100 {
+        let document = bson::decode_document(&mut file).unwrap();
+        println!("{:?}", document);
+    }
+}
+
+fn serialize() {
+    for i in 0..100 {
+        let a = Move {
+            direction: match i % 4 {
+                0 => Direction::South,
+                1 => Direction::North,
+                2 => Direction::East,
+                3 => Direction::West,
+                _ => Direction::South,
+            },
+        };
+
+        let serialized = bson::to_bson(&a).unwrap();
+
+        let mut buf = Vec::new();
+        if let bson::Bson::Document(document) = serialized {
+            bson::encode_document(&mut buf, &document);
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .append(true)
+                .open("bson")
+                .unwrap();
+            file.write_all(&buf);
+        }
+    }
 }
